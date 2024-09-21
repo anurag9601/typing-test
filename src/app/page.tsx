3ffc3495cd5f8bf6f146308119b36e404f49250c
anurag9601 <anuragmishrap13@gmail.com>
2024-09-21 23:26:03 +0530

    Home ui part done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,39 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Navbar from "./components/navbar/navbar";
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { words } from "./_words/words";
+import style from "./page.module.css";
 
 export default function Home() {
+  const [state, setState] = useState<string[]>([]);
+  const countState = useRef<number>(0);
+  const wordRef = useRef(null);
+  const letterRef = useRef(null);
+
+  useEffect(() => {
+    while (countState.current != 200) {
+      const randomInt = Math.floor(Math.random() * words.length - 1) + 0;
+      setState((prev: string[]) => [...prev, words[randomInt]]);
+      countState.current += 1;
+    }
+  }, []);
+
   return (
-    <div>
-      <Navbar />
+    <div className={style.homeContainer}>
+      <div className={style.timeOption}>
+        <div>30</div>
+        <div>50</div>
+        <div>60</div>
+        <div>100</div>
+      </div>
+      <div className={style.wordsContainer}>
+        {state.map((words, index) => (
+          <div key={index} ref={wordRef}>
+            {words?.split("").map((letters, index) => (
+              <span ref={letterRef}>{letters}</span>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,26 +1,35 @@
+"use client";
+
 import style from "./navbar.module.css";
 import Image from "next/image";
 import logo from "../../assets/images/keyboard.png";
 import friends from "../../assets/images/laugh.png";
 import notification from "../../assets/images/notification.png";
-import user from "../../assets/images/notification.png";
+import user from "../../assets/images/user.png";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const router = useRouter();
   return (
     <div className={style.navbarContainer}>
-      <div className={style.logo}>
-        <Image src={logo} alt="" />
+      <div className={style.logoContainer} onClick={() => router.push("/")}>
+        <Image src={logo} alt="" className={style.logo} />
         <h1>Typist</h1>
       </div>
       <div className={style.moreOptions}>
         <div className={style.navOption}>
-          <Image src={friends} alt="" />
+          <Image
+            src={friends}
+            alt=""
+            className={style.option}
+            onClick={() => router.push("/friends")}
+          />
         </div>
         <div className={style.navOption}>
-          <Image src={notification} alt="" />
+          <Image src={notification} alt="" className={style.option} />
         </div>
         <div className={style.navOption}>
-          <Image src={user} alt="" />
+          <Image src={user} alt="" className={style.option} />
         </div>
       </div>
     </div>
