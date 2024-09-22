a27f26de9a02b672c699d1b55bb70b77818903e2
anurag9601 <anuragmishrap13@gmail.com>
2024-09-22 21:05:22 +0530

    Add cursor event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import "./word.css";
 export default function Home() {
   const [state, setState] = useState<string[]>([]);
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const [time, setTime] = useState<number | undefined>();
 
   const countState = useRef<number>(0);
 
@@ -21,6 +22,38 @@ export default function Home() {
   const currentWordRef = useRef<HTMLDivElement | null>(null);
 
   const paragraphRef = useRef<HTMLDivElement | null>(null);
+  const cursorRef = useRef<HTMLDivElement | null>(null);
+  const mainParagraphRef = useRef<HTMLDivElement | null>(null);
+
+  const updateCursorPosition = () => {
+    const cursor = cursorRef.current;
+    const paragraph = paragraphRef.current;
+    const nextLetter = currentWordRef.current?.children[letterIndex.current];
+
+    if (nextLetter && paragraph && cursor) {
+      const rect = nextLetter.getBoundingClientRect();
+      const containerRect = paragraph.getBoundingClientRect();
+
+      const offsetLeft = rect.left - 2 - containerRect.left;
+      const offsetTop = rect.top + 5 - containerRect.top;
+
+      cursor.style.left = `${offsetLeft - 2}px`;
+      cursor.style.top = `${offsetTop}px`;
+    } else {
+      const currentWord = currentWordRef.current;
+      if (currentWord && currentWordLength.current && cursor && paragraph) {
+        const lastLetter = currentWord.children[currentWordLength.current - 1];
+        const rect = lastLetter.getBoundingClientRect();
+        const containerRect = paragraph.getBoundingClientRect();
+
+        const offsetLeft = rect.right - containerRect.left;
+        const offsetTop = rect.top - containerRect.top;
+
+        cursor.style.left = `${offsetLeft - 2}px`;
+        cursor.style.top = `${offsetTop + 5}px`;
+      }
+    }
+  };
 
   const handleRestart = () => {
     countState.current = 0;
@@ -48,9 +81,10 @@ export default function Home() {
       const isBackspace = e.key == "Backspace";
 
       if (isBackspace) {
-        if (extraTypeLetter.current > 0) {
+        if (extraTypeLetter.current !== 0) {
           currentWordRef.current?.lastChild?.remove();
           extraTypeLetter.current -= 1;
+          updateCursorPosition();
         } else {
           if (letterIndex.current == 0) return;
           letterIndex.current -= 1;
@@ -60,6 +94,7 @@ export default function Home() {
           currentWordRef.current?.children[
             letterIndex.current
           ]?.classList.remove("right");
+          updateCursorPosition();
         }
       }
 
@@ -70,6 +105,13 @@ export default function Home() {
         currentWordRef.current = wordRef.current[wordIndex.current];
         currentWordLength.current =
           wordRef.current[wordIndex.current]?.children.length;
+        if (mainParagraphRef.current!.getBoundingClientRect().top > 216) {
+          console.log(mainParagraphRef.current!.getBoundingClientRect());
+          const margin = parseInt(mainParagraphRef.current!.style.top || "0px");
+          mainParagraphRef.current!.style.top = margin - 60 + "px";
+          updateCursorPosition();
+        }
+        updateCursorPosition();
       }
 
       if (e.key.match(/^[a-z]+$/) && !isBackspace) {
@@ -83,11 +125,13 @@ export default function Home() {
                 letterIndex.current
               ].classList.add("right");
               letterIndex.current += 1;
+              updateCursorPosition();
             } else {
               currentWordRef.current?.children[
                 letterIndex.current
               ].classList.add("wrong");
               letterIndex.current += 1;
+              updateCursorPosition();
             }
           } else {
             const span = document.createElement("span");
@@ -96,6 +140,7 @@ export default function Home() {
             span.classList.add("extra");
             currentWordRef.current?.append(span);
             extraTypeLetter.current += 1;
+            updateCursorPosition();
           }
         }
       }
@@ -112,18 +157,40 @@ export default function Home() {
     currentWordRef.current = wordRef.current[wordIndex.current];
     currentWordLength.current =
       wordRef.current[wordIndex.current]?.children.length;
+    updateCursorPosition();
   }, [state]);
 
   return (
     <div className={style.homeContainer}>
       <div className={style.timeOption}>
-        <div>30</div>
-        <div>50</div>
-        <div>60</div>
-        <div>100</div>
+        <div
+          onClick={() => setTime(30)}
+          className={`${time == 30 && style.setTime}`}
+        >
+          30
+        </div>
+        <div
+          onClick={() => setTime(50)}
+          className={`${time == 50 && style.setTime}`}
+        >
+          50
+        </div>
+        <div
+          onClick={() => setTime(60)}
+          className={`${time == 60 && style.setTime}`}
+        >
+          60
+        </div>
+        <div
+          onClick={() => setTime(100)}
+          className={`${time == 100 && style.setTime}`}
+        >
+          100
+        </div>
       </div>
       <div className={style.wordsMainContainer} ref={paragraphRef}>
-        <div className={style.wordsContainer}>
+        <div className={style.wordsContainer} ref={mainParagraphRef}>
+          <div className={style.cursor} ref={cursorRef}></div>
           {state.map((words, index) => (
             <div
               key={index}
@@ -131,6 +198,7 @@ export default function Home() {
                 wordRef.current[index] = e;
               }}
               tabIndex={0}
+              className={style.word}
             >
               {words?.split("").map((letters, letterIndex) => (
                 <span key={letterIndex}>{letters}</span>
